49c8d0d9e87329bfd199b8c513ab310284dd120a
Naran Gomes <gom3snaran@gmail.com>
2022-01-15 20:15:27 -0300

    Criado metodo Roteamento na classe Startup

--- Asp.NET_Core_Uma_webapp_usando_o_padrao_MVC/Alura.ListaLeitura/Alura.ListaLeitura.App/Startup.cs
+++ Asp.NET_Core_Uma_webapp_usando_o_padrao_MVC/Alura.ListaLeitura/Alura.ListaLeitura.App/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using Alura.ListaLeitura.App.Repositorio;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Alura.ListaLeitura.App
@@ -9,13 +10,29 @@ namespace Alura.ListaLeitura.App
     {
         public void Configure(IApplicationBuilder app)
         {
-            app.Run(LivrosParaLer);
+            app.Run(Roteamento);
         }
-
-        public Task LivrosParaLer(HttpContext context)
+        public Task Roteamento(HttpContext context)
         {
             var _repo = new LivroRepositorioCSV();
-            return context.Response.WriteAsync(_repo.ParaLer.ToString());
+            var caminhosAtendidos = new Dictionary<string, string>
+            {
+                {"/Livros/ParaLer", _repo.ParaLer.ToString()},
+                {"/Livros/Lendo", _repo.Lendo.ToString()},
+                {"/Livros/Lidos", _repo.Lidos.ToString()}
+            };
+
+            if (caminhosAtendidos.ContainsKey(context.Request.Path))
+            {
+                return context.Response.WriteAsync(caminhosAtendidos[context.Request.Path]);
+            }
+
+            return context.Response.WriteAsync(context.Request.Path);
+        }
+        public Task LivrosParaLer(HttpContext context)
+        {
+            
+            return context.Response.WriteAsync(context.Request.Path);
         }
     }
 }
