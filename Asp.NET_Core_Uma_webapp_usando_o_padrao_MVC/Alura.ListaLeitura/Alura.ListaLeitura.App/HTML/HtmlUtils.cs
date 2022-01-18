2ebaae6b071c4129ce6ee004786068d90f8ca311
Naran Gomes <gom3snaran@gmail.com>
2022-01-18 16:24:12 -0300

    Criadas classes LivrosLogica e CadastroLogica para dividir as responsabilidades da classe Startup

---
+++ Asp.NET_Core_Uma_webapp_usando_o_padrao_MVC/Alura.ListaLeitura/Alura.ListaLeitura.App/HTML/HtmlUtils.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+
+namespace Alura.ListaLeitura.App.HTML
+{
+    public class HtmlUtils
+    {
+        public static string CarregaArquivoHTML(string nomeArquivo)
+        {
+            var nomeCompletoDoArquivo = $"HTML/{nomeArquivo}.html";
+            using (StreamReader arquivo = File.OpenText(nomeCompletoDoArquivo))
+            {
+                return arquivo.ReadToEnd();
+            }
+
+        }
+    }
+}
